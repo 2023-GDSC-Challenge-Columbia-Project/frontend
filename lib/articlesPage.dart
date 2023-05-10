8f3f5e2c70cb475245f8b7b4b893965e284379f8
Byeolah-Kwon <119546622+Byeolah-Kwon@users.noreply.github.com>
2023-05-10 16:27:01 -0400

    title case update

--- lib/articlesPage.dart
+++ lib/articlesPage.dart
@@ -1,166 +1,71 @@
 import 'main.dart';
 import 'favArticlesPage.dart';
 import 'package:flutter/material.dart';
-//import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:provider/provider.dart'; //for watch MyAppState
+import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class RelatedArticlesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
-    var appState = context.watch<MyAppState>();
-    int pw = appState.pregnancy_week;
-    IconData icon;
-    IconData? _getIcon (link){
-    if (appState.favorites.contains(link)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-    return icon;}
-    //dummy comment ignore
-    String capitalize(String value) {
-    var result = value[0].toUpperCase();
-    bool cap = true;
-    for (int i = 1; i < value.length; i++) {
-      if (value[i - 1] == " " && cap == true) {
-        result = result + value[i].toUpperCase();
-      } 
-      else {
-            result = result + value[i];
-            cap = false;
-      }
-    }
-    return result;
-  }
-
-    
-    return Scaffold(
-      backgroundColor: colorScheme.surfaceVariant,
-      appBar: AppBar(
-        title: Text("Search for your information!"),
-        leading: Icon(Icons.book),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => FavArticlesPage()));}, 
-            icon: const Icon(Icons.arrow_forward),
-            tooltip: "Open favorite articles page"),
-          IconButton(
-            onPressed: () {Navigator.push(
-                    context, MaterialPageRoute(builder: (_) => FavArticlesPage()));}, 
-            icon: const Icon(Icons.arrow_forward),
-            tooltip: "Open favorite articles page")
-        ],
-        
-      ),
-      
-      body: 
-    
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-                
-            Expanded(
-              // Make better use of wide windows with a grid.
-              child:           
-                ListView(
-                  children: [
-                    for (var link in appState.allLinks)             
-                    
-                      ListTile(
-                        leading: IconButton(
-                          icon: Icon(_getIcon(link), semanticLabel: 'Bookmark'),
-                          color: colorScheme.primary,
-                          onPressed: () {
-                            appState.toggleFavorite(link);
-                          },
-                        ) ,
-                        title: Text(
-                          capitalize(link.split('/').last.replaceAll('-', ' '))
-                          //pair.asLowerCase,
-                          //semanticsLabel: pair.asPascalCase,
-                        ),
-                        onTap: () => launch("$link")
-                      ),
-                  ],
-                ),
-            ),
-          ],
-      ),
-    ); 
-  }
-}
-
-/*class RelatedArticlesPage extends StatefulWidget {
-  @override
-  _RelatedArticlesPageState createState() => _RelatedArticlesPageState();
-}
-
-class _RelatedArticlesPageState extends State<RelatedArticlesPage> {
-
-final Uri _url = Uri.parse('https://flutter.dev');
-
-  @override
-  Widget build(BuildContext context) {
-    
-     return Scaffold(
-      
-        body: SingleChildScrollView(
+class DashboardPage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      var colorScheme = Theme.of(context).colorScheme;
+      return SingleChildScrollView(
         child: Column(
           
-          children: <Widget>
-          
-          [
+          children: <Widget>[
 
             Padding(
-            padding: const EdgeInsets.only(top: 40.0)),
+            padding: const EdgeInsets.only(top: 110.0)),
+
+            Center(child: Text('Dashboard',
+            style: TextStyle(fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ))),
+
+            Padding(
+            padding: const EdgeInsets.only(top: 10.0)),
 
             Container(
-              height: 40,
+              height: 150,
               width: 400,
               decoration: BoxDecoration(
-                  color: Colors.lime, borderRadius: BorderRadius.circular(5)),
-            
-                child: InkWell(
-                  child: Text ('8 Pregnancy Questions to Ask When Unexpectedly Pregnant', 
-                  textAlign: TextAlign.center, style:TextStyle(fontSize: 15)),
-                  onTap:() => launch('https://www.availnyc.org/8-questions-unexpected-pregnancy')
-                )
+                  color: Colors.lime),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => MyHomePage()));
+                }, style: ElevatedButton.styleFrom(
+                  primary: Colors.white
+                 ),
+                child: Text(
+                  'Your Favorite Nearby Resources',
+                  style: TextStyle(fontSize: 20, color: Colors.brown),
+                ),
               ),
+            ),
 
             Padding(
-            padding: const EdgeInsets.only(top: 20.0)),
+            padding: const EdgeInsets.only(top: 10.0)),
 
-              Container(
-              height: 40,
+            Container(
+              height: 150,
               width: 400,
               decoration: BoxDecoration(
-                  color: Colors.lime, borderRadius: BorderRadius.circular(5)),
-            
-                child: InkWell(
-                  child: Text ('8 Tips to Process an Accidental Pragnency', 
-                  textAlign: TextAlign.center, style:TextStyle(fontSize: 15)),
-                  onTap:() => launch('https://www.availnyc.org/8-tips-to-process-an-accidental-pregnancy')
-                )
+                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => FavArticlesPage()));
+                }, style: ElevatedButton.styleFrom(
+                  primary: Colors.white
+                 ),
+                child: Text(
+                  'Your Favorite Articles',
+                  style: TextStyle(fontSize: 20, color: Colors.brown),
+                ),
               ),
-            
-
-          ]
-
-        )
-        )
+            ),
+          ],
+        ),
       );
     }
-
-    Future<void> _launchUrl() async {
-     if (!await launchUrl(_url)) {
-     throw Exception('Could not launch $_url');
-     }
-   }
-
-
-   use Tile List and Favorite button 
   }
-  */
